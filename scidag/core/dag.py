96fbda7fa6251db53b9acc48e4348b6d1fcead23
markmno <nosanmark@gmail.com>
2023-01-29 12:43:01 +0200

    change methods and add few things to DAG

--- scidag/core/dag.py
+++ scidag/core/dag.py
@@ -2,13 +2,13 @@ from typing import Any
 
 from omegaconf import DictConfig
 
-from scidag.core._runnable import Runnable
 from scidag.core.task import Task
+from scidag.utils.runnable import Runnable
 
 
 class DAG(Runnable):
     """
-    Class to manupulate dags
+    Directed Acyclic Graph
     """
 
     def __init__(self, cfg: DictConfig) -> None:
@@ -21,19 +21,21 @@ class DAG(Runnable):
         self.tasks: dict[str, Task] = build_tasks()
 
     @classmethod
-    def build_from_cfg(cls, cfg: DictConfig):
-        """
-        Build `DAG` by provided config
+    def from_config(cls, cfg: DictConfig):
+        r"""
+        Build :class:`DAG` by provided config
 
         Parameters
         ----------
         cfg : DictConfig
             _description_
         """
+        # TODO: Unpack config to the DAG
+        return DAG()
 
-    def get_task(self, name: str) -> Task:
+    def get_task(self, task_name: str) -> Task:
         """Get Task by its name"""
-        return self.tasks[name]
+        return self.tasks[task_name]
 
     def append(self, task: Task, dependencies: dict[str, Any]):
         """
@@ -48,7 +50,25 @@ class DAG(Runnable):
         """
         # Check if task can be appended to parent nodes and its outputs is also specified
 
+    def get_available_tasks(self, task_name: str) -> list[str] | None:
+        r"""
+        Shows available tasks to add after this task
+
+        Returns
+        -------
+        list[str] | None
+            Available tasks after this task
+        Example
+        -------
+        """
+        # Find things in StorageGraph that
+
     def run(self) -> None:
-        """run dag with logging and saving in hydra"""
+        """Run dag with logging and saving configuration file"""
         for task in self.tasks.values():
             task.run()
+
+    def save(self):
+        """Save DAG configuration to specific format"""
+        for task in self.task.values():
+            task.save()
