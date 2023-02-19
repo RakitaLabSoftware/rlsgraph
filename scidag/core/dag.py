78cd2aa2e1432778a51017681a17193e27edf48a
markmno <nosanmark@gmail.com>
2023-02-19 11:59:44 +0200

    reworked dag

--- scidag/core/dag.py
+++ scidag/core/dag.py
@@ -1,13 +1,11 @@
 import asyncio
-from typing import Any
 
-import uvloop
+import networkx as nx
 from omegaconf import DictConfig
 
 from scidag.core.node import Node
 from scidag.utils.runnable import BaseElement
-
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+from scidag.utils.storage import DataStorage
 
 
 class DAG(BaseElement):
@@ -15,17 +13,14 @@ class DAG(BaseElement):
     Directed Acyclic Graph
     """
 
-    def __init__(self, cfg: DictConfig) -> None:
-        """
-        Parameters
-        ----------
-        cfg : DictConfig
-            _description_
-        """
-        self.nodes: dict[str, Node] = build_nodes()
+    def __init__(
+        self, graph: nx.DiGraph | None = None, storage: DataStorage | None = None
+    ) -> None:
+        self.graph = graph if graph is not None else nx.DiGraph()
+        self.storage = storage if storage is not None else DataStorage()
 
     @classmethod
-    def from_config(cls, cfg: DictConfig):
+    def from_config(cls, cfg: DictConfig) -> "DAG":
         r"""
         Build :class:`DAG` by provided config
 
@@ -34,29 +29,63 @@ class DAG(BaseElement):
         cfg : DictConfig
             _description_
         """
-        # TODO: Unpack config to the DAG
-        return DAG()
+        graph = build_graph(cfg)
+        storage = DataStorage()
+        return DAG(graph, storage)
 
-    def get_node(self, task_name: str) -> Node:
+    def get_node(self, node_name: str):
         """Get Task by its name"""
-        return self.nodes[task_name]
+        if node_name not in self.graph.nodes.keys():
+            raise KeyError(
+                f"Node named {node_name} is not present in this DAG. Try `dag.all_nodes` to see all available nodes"
+            )
+        return self.graph.nodes[node_name]["node"]
 
-    def append(self, node: Node, *, dependencies: dict[str, Any]):
-        r"""
-        Append New Task To current Dag with edges specified in dependencies
+    def add(self, node: Node) -> None:
+        r"""Add :class:`scidag.Node` to pool of nodes"""
+        self.graph.add_node(node.name, node=node)
+        self.storage.add_node(node.name, node=node)
+
+    def remove(self, node: Node) -> None:
+        r"""Remove :class:`scidag.Node` from this dag"""
+        self.graph.remove_node(node.name)
+        self.storage.remove_node(node.name)
+
+    def connect(self, u_node_name: str, v_node_name: str) -> None:
+        """
+        Connect dependencies of two nodes
 
         Parameters
         ----------
-        task : Task
+        u_node_name : str
             _description_
-        dependencies : dict[str, Any]
+        v_node_name : str
             _description_
         """
-        # TODO: Make also possible to add stuff by wrappers
-        # Check if task can be appended to parent nodes and its outputs is also specified
-        # IF dependencies are multiple then await asyncio.gather(*all tasks)
+        # get nodes
+        v_node_name, target_variable_name = v_node_name.split(".")
+        v_node = self.get_node(v_node_name)
+        u_node = self.get_node(u_node_name)
 
-    def get_available_tasks(self, task_name: str) -> list[str] | None:
+        # add edge to graph
+        self.graph.add_edge(
+            u_node.name, v_node.name, target_variable=target_variable_name
+        )
+        self.storage.add_edge(
+            u_node.name, v_node.name, target_variable=target_variable_name
+        )
+
+    def disconnect(self, u_node_name: str, v_node_name: str) -> None:
+        # get nodes
+        v_node_name, v_node_prefix = v_node_name.split(".")
+        v_node = self.get_node(v_node_name)
+        u_node = self.get_node(u_node_name)
+
+        # remove edge from self.graph
+        self.graph.remove_edge(u_node.name, v_node.name)
+        self.storage.remove_edge(u_node.name, v_node.name)
+
+    def get_available_nodes(self, node_name: str) -> list[str] | None:
         r"""
         Shows available tasks to add after this task
 
@@ -64,26 +93,20 @@ class DAG(BaseElement):
         -------
         list[str] | None
             Available tasks after this task
-
-        Example
-        -------
         """
         # Find things in StorageGraph that
+        # 1. Find disconnected notes
+        # 2. Match it dependencies type with node_name
+        pass
 
-    async def run(self):
+    def __call__(self) -> None:
         """Run dag with logging and saving configuration file"""
-        # TODO: CREATE AN EVENTLOOP AND RUN IT ASYNCRONIOUSLY
-        # initialise eventloop
-        loop = asyncio.get_event_loop()
-        loop.run()
-        for task in self.tasks.values():
-            await task.run()
-        asyncio.run(self._main())
+        asyncio.run(self.run())
 
-    def save(self):
-        """Save DAG configuration to specific format"""
-        for task in self.task.values():
-            task.save()
+    async def run(self) -> None:
+        tasks = list(nx.topological_sort(self.graph))
+        await asyncio.gather(*[task.run() for task in tasks])
 
-    def show(self):
-        """Visualise Graph"""
+    @property
+    def all_nodes(self):
+        return list(self.graph.nodes.keys())
